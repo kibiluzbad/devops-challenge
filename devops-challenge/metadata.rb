7875cecc8a594c1122b8ba8d8d8ecc591aae4e1d
Leonardo Cardoso <kibiluzbad@gmail.com>
2018-02-25 18:45:53 -0300

    Fix metadata

--- devops-challenge/metadata.rb
+++ devops-challenge/metadata.rb
@@ -1,17 +1,13 @@
 name 'devops-challenge'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
+maintainer 'Leonardo Cardoso'
+maintainer_email 'kibiluzbad@gmail.com'
+license 'All rights reserved'
 description 'Installs/Configures devops-challenge'
 long_description 'Installs/Configures devops-challenge'
-version '0.1.0'
+version '0.2.0'
+source_url 'git@github.com:kibiluzbad/devops-challenge.git'
+issues_url 'https://github.com/kibiluzbad/devops-challenge/issues'
+chef_version '>= 12.5' if respond_to?(:chef_version)
+supports 'centos'
 depends 'java', '~> 1.50.0'
 depends 'tomcat', '~> 3.0.0'
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/devops-challenge/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/devops-challenge' if respond_to?(:source_url)
